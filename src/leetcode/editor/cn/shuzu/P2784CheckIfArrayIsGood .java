8c500938e147d314b004d5974becb45a5ddef814
blaze <1466528493@qq.com>
2026-05-14 14:29:43 +0800

    2026.05.14

---
+++ src/leetcode/editor/cn/shuzu/P2784CheckIfArrayIsGood .java
@@ -0,0 +1,32 @@
+package leetcode.editor.cn.shuzu;
+
+import java.util.Arrays;
+
+class P2784CheckIfArrayIsGood{
+    public static void main(String[] args){
+        Solution solution = new P2784CheckIfArrayIsGood().new Solution();
+        int[] nums = {1, 3, 3, 2};
+        System.out.println(solution.isGood(nums));
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isGood(int[] nums) {
+        int n = Arrays.stream(nums).max().getAsInt(); //获取最大值
+        if(n != nums.length - 1){
+            return false;
+        }
+        int[] cnt = new int[n + 1];
+        for (int num : nums) {
+            cnt[num]++;
+        }
+        for (int i = 1; i < n; i++) {
+            if (cnt[i] != 1) {
+                return false;
+            }
+        }
+        return cnt[n] == 2;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
